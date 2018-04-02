84011d805a234d80938b35d041c3843bacc71e61
Daniel Glinka <kaitsh@d-git.de>
2018-04-02 06:23:41 +0200

    read xml and create verse

--- io.go
+++ io.go
@@ -1,29 +1,54 @@
 package main
 
 import (
+	"encoding/xml"
+	"fmt"
 	"io/ioutil"
-	"time"
+	"net/http"
+	"path"
 )
 
-type Verse struct {
-	Date        time.Time    `xml:Datum`
-	Day         time.Weekday `xml:Wtag`
-	Sunday      string       `xml:Sonntag`
-	Text        string       `xml:Losungstext`
-	Reference   string       `xml:Losungsvers`
-	Teaching    string       `xml:Lehrtext`
-	TeachingRef string       `xml:Lehrtextvers`
-	License     string       `xml:License`
+type xmlInput struct {
+	MetaData string  `xml:"FreeXml"`
+	Data     []Verse `xml:"Losungen"`
 }
 
 func fileReader(path string) ([]byte, error) {
 	bytes, err := ioutil.ReadFile(path)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	return bytes
+	return bytes, nil
 }
 
+// ImportVerses loads all verses from a directory and returns them as slice
 func ImportVerses(dirPath string) ([]Verse, error) {
-	panic("not implemented")
+	files, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		return nil, err
+	}
+
+	verses := make([]Verse, 0)
+	for _, f := range files {
+		bytes, err := fileReader(path.Join(dirPath, f.Name()))
+		if err != nil {
+			return nil, err
+		}
+
+		if http.DetectContentType(bytes) != "text/plain; charset=utf-8" {
+			fmt.Printf("%v is not a valid xml document\n", f.Name())
+			continue
+		}
+
+		data := xmlInput{}
+		err = xml.Unmarshal(bytes, &data)
+		if nil != err {
+			return nil, err
+		}
+		for index, verse := range data.Data {
+			data.Data[index].Day = verse.Date.Weekday()
+		}
+		verses = append(verses, data.Data...)
+	}
+	return verses, nil
 }
