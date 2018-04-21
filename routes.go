63f726ab1f042c484d248cac53d28b05d378f837
Daniel Glinka <kaitsh@d-git.de>
2018-04-21 13:08:47 +0200

    change route prefix to /v

--- routes.go
+++ routes.go
@@ -7,8 +7,8 @@ import (
 // Routes are all api routes and their handlers
 func Routes(server *echo.Echo) {
 	server.Static("/", "public")
-	server.GET("/api/:yyyy", Year)
-	server.GET("/api/:yyyy/:mm", Month)
-	server.GET("/api/:yyyy/:mm/:dd", Day)
-	server.GET("/api/today", Today)
+	server.GET("/v/:yyyy", Year)
+	server.GET("/v/:yyyy/:mm", Month)
+	server.GET("/v/:yyyy/:mm/:dd", Day)
+	server.GET("/v/", Today)
 }
